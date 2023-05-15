e4f6dd8a57dc009421740452d8f9416bba4b7ca5
Tianze <yutianzeabc@126.com>
2023-05-15 10:35:47 +0800

    创建IleocecalClassifier

--- Classifier/Model.py
+++ Classifier/Model.py
@@ -38,14 +38,14 @@ class SiteQualityClassifier(ResNet50Classifier):
             'pred_fine_gt_outside': (torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
             'pred_nonsense_gt_fine': (torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 0)).float().sum(),
             'pred_nonsense_gt_nonsense': (
-                        torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
+                    torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
             'pred_nonsense_gt_outside': (
-                        torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
+                    torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
             'pred_outside_gt_fine': (torch.eq(y_hat.argmax(dim=-1), 2) & torch.eq(y.argmax(dim=-1), 0)).float().sum(),
             'pred_outside_gt_nonsense': (
-                        torch.eq(y_hat.argmax(dim=-1), 2) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
+                    torch.eq(y_hat.argmax(dim=-1), 2) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
             'pred_outside_gt_outside': (
-                        torch.eq(y_hat.argmax(dim=-1), 2) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
+                    torch.eq(y_hat.argmax(dim=-1), 2) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
         }
 
         self.log_dict(confuse_matrix, logger=True, sync_dist=True, reduce_fx=torch.sum)
@@ -62,6 +62,57 @@ class SiteQualityClassifier(ResNet50Classifier):
         self.to_torchscript(f'./ModelScript/model_{type(self)}.pt', method='trace')
 
 
+class IleocecalClassifier(ResNet50Classifier):
+    def training_step(self, batch, batch_idx: int):
+        x, y = batch  # x是图像tensor，y是对应的标签，y形如tensor([1.,0.,0.])
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True, logger=True, sync_dist=True)
+        # 计算train_acc
+        acc = (y_hat.argmax(dim=-1) == y.argmax(dim=-1)).float().mean()
+        self.log("train_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        # y_hat.argmax 之后 ileocecal是0，nofeature是1
+        # 计算ileocecal的precision
+        precision = (torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 0)).float().sum() / \
+                    (torch.eq(y_hat.argmax(dim=-1), 0).float().sum())
+        self.log('train_precision', precision, prog_bar=True, logger=True, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx: int):
+        x, y = batch  # x是图像tensor，y是对应的标签，y形如tensor([1.,0.,0.])
+        y_hat = self(x)
+        # 计算val_acc
+        acc = (y_hat.argmax(dim=-1) == y.argmax(dim=-1)).float().mean()
+        self.log('val_acc', acc, prog_bar=True, logger=True, sync_dist=True)
+        precision = (torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 0)).float().sum() / \
+                    (torch.eq(y_hat.argmax(dim=-1), 0).float().sum())
+        self.log('val_precision', precision, prog_bar=True, logger=True, sync_dist=True)
+
+        confuse_matrix: Dict[str, int] = {
+            'pred_ileocecal_gt_ileocecal': (
+                        torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 0)).float().sum(),
+            'pred_ileocecal_gt_nofeature': (
+                        torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
+            'pred_nofeature_gt_ileocecal': (
+                        torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 0)).float().sum(),
+            'pred_nofeature_gt_nofeature': (
+                        torch.eq(y_hat.argmax(dim=-1), 1) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
+        }
+        self.log_dict(confuse_matrix, logger=True, sync_dist=True, reduce_fx=torch.sum)
+
+    def test_step(self, batch, batch_idx: int):
+        x, y = batch  # x是图像tensor，y是对应的标签，y形如tensor([1.,0.,0.])
+        y_hat = self(x)
+        # 计算test_acc
+        acc = (y_hat.argmax(dim=-1) == y.argmax(dim=-1)).float().mean()
+        self.log('test_acc', acc, prog_bar=True, logger=True, sync_dist=True)
+        precision = (torch.eq(y_hat.argmax(dim=-1), 0) & torch.eq(y.argmax(dim=-1), 0)).float().sum() / \
+                    (torch.eq(y_hat.argmax(dim=-1), 0).float().sum())
+        self.log('test_precision', precision, prog_bar=True, logger=True, sync_dist=True)
+
+    def on_test_epoch_start(self):
+        os.makedirs('./ModelScript', exist_ok=True)
+        self.to_torchscript(f'./ModelScript/model_{type(self)}.pt', method='trace')
 
 
 class CleansingClassifier(ResNet50Classifier):
@@ -99,8 +150,6 @@ class CleansingClassifier(ResNet50Classifier):
             'pred_bbps3_gt_bbps1': (torch.eq(y_hat.argmax(dim=-1), 3) & torch.eq(y.argmax(dim=-1), 1)).float().sum(),
             'pred_bbps3_gt_bbps2': (torch.eq(y_hat.argmax(dim=-1), 3) & torch.eq(y.argmax(dim=-1), 2)).float().sum(),
             'pred_bbps3_gt_bbps3': (torch.eq(y_hat.argmax(dim=-1), 3) & torch.eq(y.argmax(dim=-1), 3)).float().sum()
-
-
 
         }
 
